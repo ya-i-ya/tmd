117cd535a85e347238bc928a800acab08f217a5c
Nanamimikiki <nam.prog.acc@gmail.com>
2024-12-28 06:48:39 +0300

    Added client and wrapper for future purpose

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,44 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/tg"
 	"log"
 )
 
 func main() {
-	// Создаём клиент
-	// Запускаем клиент, оборачивая всё в client.Run
-	// Проверка/выполнение аутентификации
-	// Получаем список чатов или конкретный чат
-	// (Например, настроить в конфиге, какие чаты интересуют)
-	// Загружаем сообщения и скачиваем медиа
+
+	client := telegram.NewClient(apiID, apiHash, telegram.Options{})
+
+	err := client.Run(context.Background(), func(ctx context.Context) error {
+
+		authStatus, err := client.Auth().Status(ctx)
+
+		if err != nil {
+			return fmt.Errorf("failed to get auth status: %w", err)
+		}
+
+		if !authStatus.Authorized {
+			// https://github.com/gotd/td/tree/master/examples
+			return fmt.Errorf("not authorized: you need to sign in")
+		}
+
+		tgClient := tg.NewClient(client)
+
+		log.Println("Client is authorized and ready!")
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("Error running client: %v", err)
+	}
 }
+
+// Создаём клиент
+// Запускаем клиент, оборачивая всё в client.Run
+// Проверка/выполнение аутентификации
+// Получаем список чатов или конкретный чат
+// (Например, настроить в конфиге, какие чаты интересуют)
+// Загружаем сообщения и скачиваем медиа
+// сортировочка
--- go.mod
+++ go.mod
@@ -1,3 +1,29 @@
 module tTdw
 
-go 1.23
+go 1.23.3
+
+toolchain go1.23.4
+
+require github.com/gotd/td v0.116.0
+
+require (
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/coder/websocket v1.8.12 // indirect
+	github.com/go-faster/errors v0.7.1 // indirect
+	github.com/go-faster/jx v1.1.0 // indirect
+	github.com/go-faster/xor v1.0.0 // indirect
+	github.com/gotd/ige v0.2.2 // indirect
+	github.com/gotd/neo v0.1.5 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
+	go.opentelemetry.io/otel v1.32.0 // indirect
+	go.opentelemetry.io/otel/trace v1.32.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.30.0 // indirect
+	golang.org/x/net v0.32.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	rsc.io/qr v0.2.0 // indirect
+)
