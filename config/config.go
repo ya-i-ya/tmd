4ad42e166d7684131b54908c21d5f7c958bb9254
Nanamimikiki <nam.prog.acc@gmail.com>
2024-12-29 19:41:24 +0300

    added config base

--- config/config.go
+++ config/config.go
@@ -1,1 +1,12 @@
 package config
+
+type Config struct {
+	Telegram struct {
+		PhoneNumber string `yaml:"phone_number"`
+		ApiID       int    `yaml:"api_id"`
+		ApiHash     string `yaml:"api_hash"`
+	} `yaml:"telegram"`
+	Download struct {
+		BaseDir string `yaml:"base_dir"`
+	} `yaml:"download"`
+}
