8151b9691bf3d41524071cfd4b24fe2eb5dc8343
Nanamimikiki <nam.prog.acc@gmail.com>
2024-12-29 19:51:27 +0300

    added config base

--- config/config.go
+++ config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
 type Config struct {
 	Telegram struct {
 		PhoneNumber string `yaml:"phone_number"`
@@ -9,4 +14,18 @@ type Config struct {
 	Download struct {
 		BaseDir string `yaml:"base_dir"`
 	} `yaml:"download"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
 }
