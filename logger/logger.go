4a95682f9a36faffd0f6dd5f0a45aad6fbd98783
Nanamimikiki <nam.prog.acc@gmail.com>
2025-01-02 15:35:56 +0300

    Added logs level map

--- logger/logger.go
+++ logger/logger.go
@@ -8,19 +8,31 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func SetupLogger(logPath string) error {
+func SetupLogger(logPath string, level string) error {
+	levelMap := map[string]zerolog.Level{
+		"debug": zerolog.DebugLevel,
+		"info":  zerolog.InfoLevel,
+		"warn":  zerolog.WarnLevel,
+		"error": zerolog.ErrorLevel,
+		"fatal": zerolog.FatalLevel,
+		"panic": zerolog.PanicLevel,
+	}
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if lvl, exists := levelMap[level]; exists {
+		zerolog.SetGlobalLevel(lvl)
+	}
+
 	log.Logger = log.Output(zerolog.ConsoleWriter{
 		Out: &lumberjack.Logger{
 			Filename:   logPath,
-			MaxSize:    10,
-			MaxAge:     28,
+			MaxSize:    10, // megabytes
+			MaxAge:     28, // days
 			MaxBackups: 5,
 			Compress:   true,
 		},
 		TimeFormat: time.RFC3339,
 	})
-
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
 	return nil
 }
