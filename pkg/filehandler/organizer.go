3e2a187ac90cb84ecab13855703365fa93a28ed0
Nanamimikiki <nam.prog.acc@gmail.com>
2025-01-06 10:27:59 +0300

    Refactor: added getDirPath and getFilePat "save" func also removed

--- pkg/filehandler/organizer.go
+++ pkg/filehandler/organizer.go
@@ -3,6 +3,7 @@ package filehandler
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 )
@@ -15,22 +16,31 @@ func NewOrganizer(baseDir string) *Organizer {
 	return &Organizer{BaseDir: baseDir}
 }
 
-func (o *Organizer) OrganizeAndSave(data []byte, mimeType string, chatID, mediaID int) (string, error) {
-	extension := getFileExtension(mimeType)
-	dirPath := filepath.Join(o.BaseDir, fmt.Sprintf("%d", chatID), mimeType)
-	fileName := fmt.Sprintf("%d%s", mediaID, extension)
-	fullPath := filepath.Join(dirPath, fileName)
-
+func (o *Organizer) getDirPath(mimeType string, chatID, mediaID int) (string, error) {
+	mimeDir := strings.TrimRight(mimeType, "/")
+	dirPath := filepath.Join(o.BaseDir, fmt.Sprintf("%d", chatID), mimeDir)
 	if err := ensureDir(dirPath); err != nil {
 		return "", err
 	}
 
-	if err := saveFile(fullPath, data); err != nil {
+	log.Info().
+		Str("path", dirPath).
+		Msg("Returned dir path")
+	return dirPath, nil
+}
+
+func (o *Organizer) getFilePath(mimeType string, chatID, mediaID int) (string, error) {
+	extension := getFileExtension(mimeType)
+	dirPath, err := o.getDirPath(mimeType, chatID, mediaID)
+	if err != nil {
 		return "", err
 	}
+	fileName := fmt.Sprintf("%d%s", mediaID, extension)
+	fullPath := filepath.Join(dirPath, fileName)
 
 	log.Info().
 		Str("path", fullPath).
-		Msg("File organized and saved")
+		Msg("Returned file path")
+
 	return fullPath, nil
 }
