e241ee0679c586ec7ca7b562098bc84785e42e1d
Nanamimikiki <nam.prog.acc@gmail.com>
2025-01-06 10:25:37 +0300

    no need to old save func here

--- pkg/filehandler/filehandler.go
+++ pkg/filehandler/filehandler.go
@@ -39,36 +39,3 @@ func ensureDir(path string) error {
 		Msg("Directory ensured")
 	return nil
 }
-
-func saveFile(path string, data []byte) error {
-	file, err := os.Create(path)
-	if err != nil {
-		log.Error().
-			Err(err).
-			Str("file", path).
-			Msg("Failed to create file")
-		return err
-	}
-	defer func(file *os.File) {
-		if err := file.Close(); err != nil {
-			log.Error().
-				Err(err).
-				Str("file", path).
-				Msg("Failed to close file")
-		}
-	}(file)
-
-	_, err = file.Write(data)
-	if err != nil {
-		log.Error().
-			Err(err).
-			Str("file", path).
-			Msg("Failed to write data to file")
-		return err
-	}
-
-	log.Info().
-		Str("file", path).
-		Msg("File saved successfully")
-	return nil
-}
